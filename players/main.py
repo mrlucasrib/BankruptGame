6684437a8a01cb8bb0541506362f64f03bdb8db6
Lucas Ribeiro <lmr2199@gmail.com>
2020-12-04 16:17:17 -0300

    ⚡️ Path

--- players/main.py
+++ players/main.py
@@ -1,17 +1,41 @@
 import play
 import settings
 import random
-
+import operator
 random.seed(123456)
 result = list()
+seeds = list()
+number_of_repetions = settings.NUMBER_OF_REPETIONS
 
-for i in range(300):
-  settings.seed = str(random.randint(88888,99999999))
-  result.append(play.execute("/home/lucas/Downloads/ProjectsGit/BankruptGame/game/bin/Release/net5.0/linux-x64/publish/game",
-             "/home/lucas/Documentos/gameConfig.txt",
-             settings.seed))
-  print(i)
+print("Running...")
+for i in range(number_of_repetions):
+    while True:
+        settings.SEED = random.randint(88888, 99999999)
+        if settings.SEED not in seeds:
+            seeds.append(settings.SEED)
+            break
+    result.append(play.execute(settings.GAME_PATH,
+                               settings.CONF_PATH,
+                               str(settings.SEED)))
+
+rounds_time_out = 0
+total_rounds = 0
+percent_victort = {'Impulsive': 0, 'Demanding': 0, 'Cautious': 0, 'Random': 0}
+behavor_winner = ""
+
 with open("result.csv", 'w') as f:
-  for r in result:
-    f.write(f"{r[0]},{r[1]}\n")
-  
+    for r in result:
+        f.write(f"{r[0]},{r[1]}\n")
+        if int(r[1]) >= 1000:
+            rounds_time_out += 1
+        else:
+            total_rounds += int(r[1])
+            percent_victort[r[0]] += 1
+
+print(f"Time-out rounds: {rounds_time_out}\nAverage duration of a match: {total_rounds/number_of_repetions}",
+      "Percent of victort:\n")
+for k, v in percent_victort.items():
+    print(f"{k}: {(v/number_of_repetions)*100}")
+
+print(
+    f"Behavor Winner {max(percent_victort.items(), key=operator.itemgetter(1))[0]}")
--- players/settings.py
+++ players/settings.py
@@ -1,1 +1,9 @@
-SEED = "2651655"
+import os
+from pathlib import Path
+cwd =Path.cwd()
+
+
+SEED = 0000
+GAME_PATH = str(cwd.parent.joinpath("game", "release","game"))
+CONF_PATH = str(cwd.joinpath("gameConfig.txt"))
+NUMBER_OF_REPETIONS = 5
