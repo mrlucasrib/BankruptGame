7330309c4df20690ce83e8769bb56747d96de3dc
Lucas Ribeiro <lmr2199@gmail.com>
2020-12-04 17:30:15 -0300

    🚀 Change seed

--- players/settings.py
+++ players/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 cwd =Path.cwd()
 
 
-SEED = 0000
+SEED = 123456
 GAME_PATH = str(cwd.parent.joinpath("game", "release","game"))
 CONF_PATH = str(cwd.joinpath("gameConfig.txt"))
 NUMBER_OF_REPETIONS = 300
